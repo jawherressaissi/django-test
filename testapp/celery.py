34c88f9e5c7abfaa63244758f1eae7343d9c34a8
jawherressaissi <jawher.ressaissi@esprit.tn>
2023-07-05 22:10:26 +0100

    .

--- testapp/celery.py
+++ testapp/celery.py
@@ -1,10 +1,10 @@
 from celery import Celery
 from celery.schedules import crontab
 
-app = Celery('ced',
-             broker='redis://localhost:6379/0',
-             backend='rpc://',
-             include=['ced.tasks'])
+app = Celery('testapp',
+             broker='redis://redis:6379/0',
+             backend='redis://redis:6379/0',
+             include=['testapp.tasks'])
 #app.config_from_object('django.conf:settings')
 #app.autodiscover_tasks(lambda : INSTALLED_APPS)
 
@@ -14,12 +14,7 @@ app.conf.update(
     worker_cancel_long_running_tasks_on_connection_loss=True
 )
 
-app.conf.beat_schedule = {
-    'linkedin-benchmark': {
-        'task': 'linkedin_benchmark',
-        'schedule': crontab(hour=10, minute=15, day_of_week='*')
-    },
-}
+app.conf.beat_schedule = {}
 
 app.conf.enable_utc = False
 app.conf.timezone = "Europe/Lisbon"
