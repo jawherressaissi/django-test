2facac757815187a34b61a36415dc67f0d4e295a
jawherressaissi <jawher.ressaissi@esprit.tn>
2023-07-05 23:25:07 +0100

    .

--- testapp/celery.py
+++ testapp/celery.py
@@ -1,7 +1,23 @@
 from celery import Celery
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangotest.settings')
- 
-app = Celery('testapp')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+from celery.schedules import crontab
+
+app = Celery('testapp',
+             broker='redis://redis:6379/0',
+             backend='redis://redis:6379/0',
+             include=['testapp.tasks'])
+#app.config_from_object('django.conf:settings')
+#app.autodiscover_tasks(lambda : INSTALLED_APPS)
+
+# Optional configuration, see the application user guide.
+app.conf.update(
+    result_expires=3600,
+    worker_cancel_long_running_tasks_on_connection_loss=True
+)
+
+app.conf.beat_schedule = {}
+
+app.conf.enable_utc = False
+app.conf.timezone = "Europe/Lisbon"
+
+if __name__ == '__main__':
+    app.start()
